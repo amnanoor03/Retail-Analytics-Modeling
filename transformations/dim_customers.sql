125ac4d4df7f2a2066657295cb956a963aa9fd24
Amna Noor <amnanoor4539@gmail.com>
2026-02-08 10:07:19 +0500

    chore: final project structure and documentation cleanup

--- transformations/dim_customers.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE dim_customers AS
-SELECT
-    ROW_NUMBER() OVER (ORDER BY customer_id) AS customer_key,
-    customer_id,
-    COUNT(*) as total_orders
-FROM retail_sales
-GROUP BY customer_id;
--- transformations/fact_sales.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE fact_sales AS
-SELECT
-    s.transaction_id,
-    d.customer_key,
-    s.product_name,
-    s.amount AS sale_amount,
-    s.transaction_date
-FROM retail_sales s
-JOIN dim_customers d ON s.customer_id = d.customer_id;
